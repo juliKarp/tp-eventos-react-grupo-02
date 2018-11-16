3a53e599778d756e096be161c86ff6f7f59d2410
maurocasciati <maurocasciati@gmail.com>
2018-11-16 15:48:05 -0300

    Agrego configuraciones y archivo de routeo.

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+export const EventosRoutes = () => (
+    <Router>
+        <Switch>
+
+        </Switch>
+    </Router>
+)
