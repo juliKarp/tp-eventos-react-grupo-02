09a4d53cd03494448db271b12a0bfbf760bffe8f
maurocasciati <maurocasciati@gmail.com>
2018-11-25 19:13:28 -0300

    Cambio en vista comprar entrada

--- src/components/comprarEntrada.js
+++ src/components/comprarEntrada.js
@@ -1,14 +1,21 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
 import DetallesEvento from './detallesEvento';
-import { Button } from '@material-ui/core';
+import { Button, Grid, IconButton } from '@material-ui/core';
 import { comprarEntrada } from '../services/eventoService';
 import LoadingIndicator from './LoadingIndicator';
 import ErrorBar from './errorBar';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+
 
 export class ComprarEntrada extends Component {
     constructor(props) {
         super(props)
-        this.state = { loading: false }
+        this.state = {
+            loading: false,
+            cantidadDeseada: 0
+        }
     }
 
     componentDidMount() {
@@ -16,6 +23,18 @@ export class ComprarEntrada extends Component {
         if (!evento) {
             history.push('/')
         }
+    }
+
+    incrementar() {
+        this.cambiarContador(this.state.cantidadDeseada + 1)
+    }
+
+    decrementar() {
+        this.cambiarContador(this.state.cantidadDeseada - 1)
+    }
+
+    cambiarContador(n) {
+        this.setState({ loading: false, cantidadDeseada: n })
     }
 
     async compraEntrada(evento) {
@@ -29,15 +48,37 @@ export class ComprarEntrada extends Component {
             this.setState({ loading: false })
         }
     }
+
     render() {
         const { location: { evento }, history } = this.props;
         const { loading, error } = this.state
-        return <Fragment>
-            {evento && <DetallesEvento evento={evento} />}
-            <Button variant="contained" onClick={() => history.push('/')}>Volver</Button>
-            <Button variant="contained" color="primary" onClick={() => this.compraEntrada(evento)}>Comprar</Button>
+        return <Grid container spacing={24}>
+            <Grid item xs={12}>
+                {evento && <DetallesEvento evento={evento} />}
+            </Grid>
+            <Grid item xs={6}>
+                <IconButton color="primary" onClick={() => this.decrementar()}>
+                    <RemoveCircleIcon />
+                </IconButton>
+                {this.state.cantidadDeseada}
+                <IconButton color="primary" onClick={() => this.incrementar(evento)}>
+                    <AddCircleIcon />
+                </IconButton>
+            </Grid>
+            <Grid item xs={6}>
+                <IconButton>
+                    <AttachMoneyIcon />
+                    {this.state.cantidadDeseada * evento.precio}
+                </IconButton>
+            </Grid>
+            <Grid item xs={6}>
+                <Button variant="contained" onClick={() => history.push('/')}>Volver</Button>
+            </Grid>
+            <Grid item xs={6}>
+                <Button variant="contained" color="primary" onClick={() => this.compraEntrada(evento)}>Comprar</Button>
+            </Grid>
             <LoadingIndicator loading={loading} />
-            <ErrorBar error={error}/>
-        </Fragment>
+            <ErrorBar error={error} />
+        </Grid>
     }
 }
